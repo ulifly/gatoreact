50bd64015e4732c32ccd1aa2567f74cc83323cc0
Ulifly <ulifly@hotmail.com>
2024-01-09 08:12:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square({value, onSquareClick}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
+ 
 }
 
-export default App;
+
+export default function Board() {
+  const [ squares, setSquares ] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
+  return(
+  <>
+    <div className="board-row">
+      <Square value={squares[0]} onSquareClick={handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={handleClick}/>
+      <Square value={squares[2]} onSquareClick={handleClick}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[3]} onSquareClick={handleClick}/>
+      <Square value={squares[4]} onSquareClick={handleClick}/>
+      <Square value={squares[5]} onSquareClick={handleClick}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} onSquareClick={handleClick}/>  
+      <Square value={squares[7]} onSquareClick={handleClick}/>  
+      <Square value={squares[8]} onSquareClick={handleClick}/>  
+    </div>
+  </>  
+  )
+}
